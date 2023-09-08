e945e090e36c8ca15da6cb1cd5328b980370e641
Alec Petridis <chop@quenda.net>
2023-09-07 18:13:18 -0700

    Revert "Make linux do the raw thing"
    This reverts commit 79ec498d6d496326c218b04005abbc2a6f0c8a85.

--- ax.xz.raw.posix/src/main/c/posix_raw.c
+++ ax.xz.raw.posix/src/main/c/posix_raw.c
@@ -31,7 +31,7 @@ int open_tun(char *name, int nameLength) {
 
 	struct ifreq ifr;
 	memset(&ifr, 0, sizeof(ifr));
-	ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+	ifr.ifr_flags = IFF_TUN;
 	int err = ioctl(fd, TUNSETIFF, &ifr);
 	if (err < 0) {
 		close(fd);
