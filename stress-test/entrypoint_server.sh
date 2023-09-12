33e4c8deeba06eb53cdc9e2934cd43cc9d16aa94
Alec Petridis <chop@quenda.net>
2023-09-12 03:50:10 -0700

    whoops i forgot to route vpn traffic through the tunnel

---
+++ stress-test/entrypoint_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+/app/run.sh "$@"&
+JAVA_PID=$!
+
+# wait for tun0 to be created (check by doing ip addr show dev tun0 and checking the exit code) or for the java process to die
+while ! ip addr show dev tun0 > /dev/null 2>&1 && kill -0 $JAVA_PID; do
+	sleep 0.1
+done
+
+# if the java process died, exit
+if ! kill -0 $JAVA_PID; then
+	exit 1
+fi
+
+OLD_GW=$(ip route show | grep default | cut -d' ' -f 3)
+ip link set dev tun0 up
+ip route add 10.0.0.0/24 dev tun0
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+echo "Masquerading all traffic through eth0"
+
+wait $JAVA_PID
